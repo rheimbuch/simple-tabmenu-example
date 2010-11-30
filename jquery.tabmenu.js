882291ef58e0243b737823866c048f767883dc83
Ryan Heimbuch <rheimbuch@gmail.com>
2010-11-29 23:03:40 -0700

    simple example of decoupled menu/tabs and content areas

---
+++ jquery.tabmenu.js
@@ -0,0 +1,50 @@
+// Wrap our plugin to ensure we can access
+// jQuery as '$' even if 'noConflict' mode is
+// enabled or some other library has stolen '$'.
+(function($){
+  // Add our plugin function to jquery
+  $.fn.tabMenu = function() {
+    
+    // Find all links in 'this' node that have href="#someId"
+    var menuLinks = this.find('a[href*="#"]');
+    
+    // Find the dom element pointed to by the 'href' attribute
+    var targetFor = function(el) {
+      var targetId = $(el).attr('href');
+      return $(targetId);
+    }
+    
+    // Resolve the link targets to elements in the page
+    var menuTargets = menuLinks.map(function(){
+      return targetFor(this).get(); // .get() strips the jquery wrapper
+    });
+    
+    // Show the content targeted by a menu link
+    var selectMenuLink = function(el) {
+      // Make all menu links "inactive"
+      menuLinks.removeClass('active');
+      
+      // Make this link 'active'
+      $(el).addClass('active');
+      
+      // hide all menu targets
+      menuTargets.hide();
+      
+      // show the target for this link
+      targetFor(el).show();
+    }
+    
+    
+    // setup click events on all menu links
+    menuLinks.each(function(){
+      $(this).click(function(){
+        selectMenuLink(this);
+        // return false to halt click event processing
+        return false;
+      });
+    });
+    
+    // show the first menu link by default
+    selectMenuLink(menuLinks.first());
+  };
+})(jQuery);
